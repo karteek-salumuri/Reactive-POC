2f50b801a824a5ff8dc55e02f9f6fecb33e28dfb
karteek salumuri <karteek.it@gmail.com>
2022-07-25 18:43:15 +0530

    Add files via upload
    files upload

---
+++ EPS.java
@@ -0,0 +1,16 @@
+package com.reactive.demo.entity;
+
+public class EPS {
+
+	private Request request;
+	
+	public Request getRequest() {
+		return request;
+	}
+
+	public void setRequest(Request request) {
+		this.request = request;
+	}
+	
+	
+}
---
+++ Request.java
@@ -0,0 +1,26 @@
+package com.reactive.demo.entity;
+
+public class Request {
+
+	private String prepaidCardNumber;
+	
+	private String mdn;
+
+	public String getPrepaidCardNumber() {
+		return prepaidCardNumber;
+	}
+
+	public void setPrepaidCardNumber(String prepaidCardNumber) {
+		this.prepaidCardNumber = prepaidCardNumber;
+	}
+
+	public String getMdn() {
+		return mdn;
+	}
+
+	public void setMdn(String mdn) {
+		this.mdn = mdn;
+	}
+	
+	
+}
